122f32a5ae0b692570344f7d9bae48e9c4e7be6b
Sebastian Carroll <scarroll@hortonworks.com>
2016-12-22 17:44:22 +0000

    Moved check_links funtion to tools

--- libs/tools.rb
+++ libs/tools.rb
@@ -102,6 +102,41 @@ task :create_post, [:title, :content, :date, :category] do |t, args|
   create_new_post(t, args)
 end
 
+def check_links
+  begin
+    require 'anemone'
+
+    root = 'http://localhost:4000/'
+    puts "Checking links with anemone ... "
+    # check-links --no-warnings http://localhost:4000
+    Anemone.crawl(root, :discard_page_bodies => true) do |anemone|
+      anemone.after_crawl do |pagestore|
+        broken_links = Hash.new { |h, k| h[k] = [] }
+        pagestore.each_value do |page|
+          if page.code != 200
+            referrers = pagestore.pages_linking_to(page.url)
+            referrers.each do |referrer|
+              broken_links[referrer] << page
+            end
+          else
+            puts "OK #{page.url}"
+          end
+        end
+        puts "\n\nLinks with issues: "
+        broken_links.each do |referrer, pages|
+          puts "#{referrer.url} contains the following broken links:"
+          pages.each do |page|
+            puts "  HTTP #{page.code} #{page.url}"
+          end
+        end
+      end
+    end
+    puts "... done!"
+
+  rescue LoadError
+    abort 'Install anemone gem: gem install anemone'
+  end
+end
 
 #
 # support functions for generating list of changed files
