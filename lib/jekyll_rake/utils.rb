c3131bbec3575514ea8f3ba25426ad7203956323
Sebastian Carroll <scarroll@hortonworks.com>
2017-01-29 13:39:53 +0000

    Added utils test

--- lib/jekyll_rake/utils.rb
+++ lib/jekyll_rake/utils.rb
@@ -1,8 +1,14 @@
 module JekyllRake
   class Utils
-    def self.slugify (title)
+    def self.slugify(title)
       # strip characters and whitespace to create valid filenames, also lowercase
       return title.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+    end
+
+    # TODO: Migrate slugify from '-' to '_'. Need to write script to migrate filenames
+    def self.slugify_lower(title)
+      # strip characters and whitespace to create valid filenames, also lowercase
+      return title.downcase.strip.gsub(' ', '_').gsub(/[^\w-]/, '')
     end
   end
 end
---
+++ test/utils_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+require 'jekyll_rake/utils'
+
+class JekyllRakeTest < Minitest::Test
+  def test_slugify_lower
+    test = "This is a Title - for testing!"
+    expected = "this_is_a_title_-_for_testing"
+    assert_equal expected, JekyllRake::Utils.slugify_lower(test)
+  end
+
+  def test_slugify
+    test = "This is a Title - for testing!"
+    expected = "this-is-a-title---for-testing"
+    assert_equal expected, JekyllRake::Utils.slugify(test)
+  end
+end
