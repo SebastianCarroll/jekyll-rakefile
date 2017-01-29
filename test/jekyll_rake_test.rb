54d2b91b0240ace0a17f1ca1da2f0110abc1dc31
Sebastian Carroll <scarroll@hortonworks.com>
2017-01-29 13:39:03 +0000

    Tidied loading on tests

--- test/jekyll_rake_test.rb
+++ test/jekyll_rake_test.rb
@@ -1,6 +1,4 @@
 require 'test_helper'
-require 'jekyll_rake'
-require 'pry'
 
 class JekyllRakeTest < Minitest::Test
   def test_that_it_has_a_version_number
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,5 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'jekyll_rake'
 
 require 'minitest/autorun'
+
+require 'pry'
