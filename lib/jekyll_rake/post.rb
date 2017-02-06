fbdaa8429bbc436cf874d2e02553b3a0e0d27695
Sebastian Carroll <scarroll@hortonworks.com>
2017-02-06 10:05:49 +0000

    More refactorings to use instance variables

--- lib/jekyll_rake/post.rb
+++ lib/jekyll_rake/post.rb
@@ -3,75 +3,66 @@ module JekyllRake
     attr_reader :file, :content, :title
 
     def initialize(t, args, dir)
-      check_title(args.title)
-      check_date(args.date)
-
-      post_title = JekyllRake::Utils.titleise(args.title)
-
-      post_date = get_post_date(args.date)
-
-      set_post_dir_and_yaml_cat(args.category, dir)
-
-      filename = get_unique_filename(post_title, post_date)
-
-      write_new_draft(filename, post_title, post_date, args)
-
+      @title = parse_title(args.title)
+      @date = parse_date(args.date)
       @content = args.content
-      @file = File.join(@post_dir, filename)
-      @title = post_title
+      @category = args.category
+
+      # TODO: This method is still out of place
+      set_post_dir_and_yaml_cat(dir)
+      @file = write_new_draft
     end
 
-    def write_new_draft(filename, post_title, post_date, args)
-      File.open(@post_dir + filename, 'w') do |f|
+    # TODO: No error handling
+    def write_new_draft()
+      filename = get_unique_filename
+      full_path = File.join(@post_dir, filename)
+      File.open(full_path, 'w') do |f|
         f.puts "---"
-        f.puts "title: \"#{post_title}\""
+        f.puts "title: \"#{@title}\""
         f.puts "layout: default"
         f.puts @yaml_cat if @yaml_cat != nil
-        f.puts "date: #{post_date}"
+        f.puts "date: #{@date}"
         f.puts "---"
         f.puts ""
-        f.puts "\# #{post_title}" # Make the heading and title the same as a default
-        f.puts args.content if args.content != nil
+        f.puts "\# #{@title}" # Make the heading and title the same as a default
+        f.puts @content if @content != nil
       end 
-      puts "Post created under \"#{@post_dir}#{filename}\""
+      puts "Post created under \"#{full_path}\""
+      full_path
     end
 
-    def get_post_date(date)
-      # TODO: What is this? Too complex
-      post_date = (date != "" and date != "nil" and not date.nil?) ? date : Time.new.strftime("%Y-%m-%d %H:%M:%S %Z")
-    end
-
-    def  get_unique_filename(post_title, post_date)
+    def  get_unique_filename()
       # TODO: Global Variable
-      filename = post_date[0..9] + "-" + JekyllRake::Utils.slugify(post_title) + $post_ext
+      filename = @date[0..9] + "-" + JekyllRake::Utils.slugify(@title) + $post_ext
 
       # TODO: refactor - very difficult to understand without the comment
       # generate a unique filename appending a number
       i = 1
       while File.exists?(@post_dir + filename) do
-        filename = post_date[0..9] + "-" +
-          File.basename(JekyllRake::Utils.slugify(post_title)) + "-" + i.to_s +
+        filename = @date[0..9] + "-" +
+          File.basename(JekyllRake::Utils.slugify(@title)) + "-" + i.to_s +
           $post_ext
         i += 1
       end
       filename
     end
 
-    def  set_post_dir_and_yaml_cat(category, dir)
-      # the destination directory is <<category>>/dir, if category is non-nil
-      # and the directory exists; dir otherwise (a category tag is added in
-      # the post body, in this case)
+    # the destination directory is <<category>>/dir, if category is non-nil
+    # and the directory exists; dir otherwise (a category tag is added in
+    # the post body, in this case)
+    def  set_post_dir_and_yaml_cat(dir)
       # Written like this to deal with category being nil
-      if category and Dir.exists?(File.join(category, dir)) then
-        @post_dir = posts
+      if @category and Dir.exists?(File.join(@category, dir)) then
+        @post_dir = File.join(@category, dir)
         @yaml_cat = nil
       else
         @post_dir = dir
-        @yaml_cat = category ? "category: #{category}\n" : nil
+        @yaml_cat = @category ? "category: #{@category}\n" : nil
       end
     end
 
-    def check_title(title)
+    def parse_title(title)
       if title == nil then
         puts "Error! title is empty"
         puts "Usage: create_post[title,content,date,category]"
@@ -80,9 +71,10 @@ module JekyllRake
         puts "CATEGORY is a string; nil or empty for no category"
         exit 1
       end
+      JekyllRake::Utils.titleise(title)
     end
 
-    def check_date(date)
+    def parse_date(date)
       # This looks cleaner but logic is incorrect
       #if (date.nil? ||
       #    date.empty? ||
@@ -95,6 +87,8 @@ module JekyllRake
         usage
         exit 1
       end
+      # TODO: What is this? Too complex
+      (date != "" and date != "nil" and not date.nil?) ? date : Time.new.strftime("%Y-%m-%d %H:%M:%S %Z")
     end
 
     def usage
@@ -104,13 +98,11 @@ module JekyllRake
       puts "CATEGORY is a string; nil or empty for no category"
       puts ""
 
-      # TODO: Broken as not passed in. Necessary?
-      title = args.title || "title"
-
-      puts "Examples: create_post[\"\",\"#{title}\"]"
-      puts "          create_post[nil,\"#{title}\"]"
-      puts "          create_post[,\"#{title}\"]"
-      puts "          create_post[#{Time.new.strftime("%Y-%m-%d")},\"#{title}\"]"
+      # TODO: Add logic here to account for nul title?
+      puts "Examples: create_post[\"\",\"#{@title}\"]"
+      puts "          create_post[nil,\"#{@title}\"]"
+      puts "          create_post[,\"#{@title}\"]"
+      puts "          create_post[#{Time.new.strftime("%Y-%m-%d")},\"#{@title}\"]"
     end
   end
 end
