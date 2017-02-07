ef9c36ff19b07949d4e6c49e2a1583f58e6984c3
Sebastian Carroll <scarroll@hortonworks.com>
2017-02-07 10:52:35 +0000

    Updated helper to pull in all files and now my coverage is more realistic as I have all files in the report

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,15 @@
 require 'simplecov'
 SimpleCov.start
 
+require 'pry'
+require 'minitest/autorun'
+
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'jekyll_rake'
 
-require 'minitest/autorun'
-
-require 'pry'
+# Added this line so SimpleCov tracks all files, not just the ones we require explicitly
+# TODO: Is this a good idea? Will always load all files which could slow things down
+# Could be temporary until we actually get a good level of tests
+Dir.glob("./lib/**/*").select{|f| File.file? f}.each{|f| require f}
 
